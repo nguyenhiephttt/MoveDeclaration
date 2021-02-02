7f6208b1aa082bb45375b2527de7e0ebea0d7985
phamhieudev <phamhieu5969@gmail.com>
2021-02-02 10:08:06 +0700

    commit

--- js/angularjs/indexController.js
+++ js/angularjs/indexController.js
@@ -30,31 +30,4 @@ app.controller('indexCtrl', function ($scope, $http) {
             $scope.employee = response.statusText;
         });
     }
-
-    // var arraytable = [];
-    // $scope.submitform = function (idProvince, datestart, dateend) {
-    //     if (datestart > dateend) {
-    //         alert("Start day cannot be greater than End day");
-    //     }
-    //     else {
-    //         var arrayta = [];
-    //         arrayta.idProvince = idProvince;
-    //         arrayta.datestart = datestart;
-    //         arrayta.dateend = dateend;
-    //         arraytable.push(arrayta);
-    //         // CLEAR TEXTBOX.
-    //         $scope.idProvince = null;
-    //         $scope.datestart = null;
-    //         $scope.dateend = null;
-    //     }
-    //     console.log(arraytable);
-    // }
-
-    // $scope.submittable = function () {
-    //     var arrMovie = [];
-    //     angular.forEach(arraytable, function (value) {
-    //         arrMovie.push('idProvince:' + value.idProvince + ', datestart:' + value.datestart + ', dateend:' + value.dateend);
-    //     });
-    //     console.log(arrMovie);
-    // }
 })
